77505bc79bb1cef2b84c50541b4242a893bf6b05
PyTerminal <124309800+PyTerminal@users.noreply.github.com>
2023-04-25 13:00:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function search(){
   var s = document.getElementById("search").value;
-  document.write("<a href='javascript:s.value();'><script>document.write(s.value);</script></a>");
+  document.write("<a href='javascript:document.getElementById('search').value'><script>document.write(s);</script></a>");
 }
